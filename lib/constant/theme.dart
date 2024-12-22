2fea87d85934ae5d824db0aa6c817e58eea64bb7
MAROOgithub <111437461+MAROOgithub@users.noreply.github.com>
2024-12-22 18:44:45 +0200

    first commit

---
+++ lib/constant/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData.dark().copyWith(
+    buttonTheme: const ButtonThemeData(),
+    scaffoldBackgroundColor: const Color(0x3C2E1F),
+  );
+
+  static ThemeData lightTheme = ThemeData.light().copyWith(
+    buttonTheme: const ButtonThemeData(),
+    iconButtonTheme: const IconButtonThemeData(),
+    scaffoldBackgroundColor: const Color(0xFFFAF6F0),
+  );
+}
